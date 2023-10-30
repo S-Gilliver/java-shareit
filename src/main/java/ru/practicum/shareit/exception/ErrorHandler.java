f7f22af44f77b91b7b47dd01990aa31d87a22014
S-Gilliver <shtyrkov.dmitry@yandex.ru>
2023-10-30 16:21:23 +0300

    add controllers

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 @RestControllerAdvice
 public class ErrorHandler {
-
     private static final Logger logger = LoggerFactory.getLogger(ErrorHandler.class);
 
     @ExceptionHandler
